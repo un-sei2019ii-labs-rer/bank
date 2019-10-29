4bdf95fbd8f0d87357f8f299ce2ced3878b1373c
Elkin Duvan Ramirez Nunez <eramirezn@unal.loc>
2019-10-29 10:54:59 -0500

    Commit app

---
+++ mobile-app/app/src/main/java/co/edu/unal/se1/presentation/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package co.edu.unal.se1.presentation.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.edu.unal.se1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
